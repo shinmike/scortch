d1f5875f0adba53b52bc63707ccd504981990935
Chris Lee <pachopa0320@gmail.com>
2017-07-14 11:37:04 -0700

    Create the toggle sidebar menu and change some styles

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -2,44 +2,83 @@ import React from 'react';
 
 class Nav extends React.Component {
 
-  render(){
+  render() {
     console.log("rendering <Nav>");
     return (
 
       <div>
 
-      <nav className="navbar navbar-light navbar-fixed-top" style={{backgroundColor: "skyblue"}} id="my-navbar">
-        <div className="container">
-          <div className="navbar-header">
-            <button type="button" className="navbar-toggle user-border" data-toggle="collapse" data-target="#navbar-collapse">
-              <span className="user-icon">
-                <i className="fa fa-user" aria-hidden="true"></i>
-              </span>
-            </button>
-            <a href="" className="navbar-brand">Scortch</a>
+        <nav className="navbar navbar-light navbar-fixed-top" style={{ backgroundColor: "skyblue" }} id="my-navbar">
+          <div className="container">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle user-border" data-toggle="collapse" data-target="#navbar-collapse">
+                <span className="user-icon">
+                  <i className="fa fa-user" aria-hidden="true"></i>
+                </span>
+              </button>
+              <a href="" className="navbar-brand">Scortch</a>
+            </div>
+            <div className="collapse navbar-collapse login-signup" id="navbar-collapse">
+              <a href="" className="btn navbar-btn navbar-right">Login</a>
+              <a href="" className="btn navbar-btn navbar-right">Sign-up</a>
+            </div>
           </div>
-          <div className="collapse navbar-collapse login-signup" id="navbar-collapse">
-            <a href="" className="btn navbar-btn navbar-right">Login</a>
-            <a href="" className="btn navbar-btn navbar-right">Sign-up</a>
-          </div>
-        </div>
-      </nav>
+        </nav>
 
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-sm-3 col-md-2 sidebar">
-            <ul className="nav nav-sidebar">
-              <li className="active"><a href="#">Overview <span className="sr-only">(current)</span></a></li>
-              <li><a href="#">NBA</a></li>
-              <li><a href="#">NBA</a></li>
-              <li><a href="#">NFL</a></li>
-              <li><a href="#">MLB</a></li>
-              <li><a href="#">MLB</a></li>
-            </ul>
+        <div className="container-fluid sideMenuBar">
+          <div className="row">
+            <div className="col-sm-3 col-md-2 sidebar">
+              <ul className="nav nav-sidebar">
+                <div id="accordion" role="tablist" aria-multiselectable="true">
+                  <div class="card">
+                    <div class="card-header" role="tab" id="headingOne">
+                      <h5 class="mb-0">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                          NBA
+                        </a>
+                      </h5>
+                    </div>
+
+                    <div id="collapseOne" class="collapse show" role="tabpanel" aria-labelledby="headingOne">
+                      <div class="card-block">
+                        hello React!
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card">
+                    <div class="card-header" role="tab" id="headingTwo">
+                      <h5 class="mb-0">
+                        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                          NFL
+                      </a>
+                      </h5>
+                    </div>
+                    <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo">
+                      <div class="card-block">
+                        hello React2
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card">
+                    <div class="card-header" role="tab" id="headingThree">
+                      <h5 class="mb-0">
+                        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                          MLB
+                        </a>
+                      </h5>
+                    </div>
+                    <div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree">
+                      <div class="card-block">
+                        hello React 3  
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
     );
   }
