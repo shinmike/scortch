ab4beb81cd86556af27aaef6ecd21a25fbc418e5
Kian <kian.akhavan@gmail.com>
2017-07-17 16:42:00 +0000

    fixed merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -45,26 +45,15 @@ class App extends React.Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
-        <Nav
 
-        />
+        <Nav />
+        <button type="button" className="btn" onClick={this.getApi}>Score!</button>
         <Dashboard
           gameTime={this.state.gameTime}
           awayTeam={this.state.awayTeam}
           homeTeam={this.state.homeTeam}
           awayScore={this.state.awayScore}
           homeScore={this.state.homeScore}
-=======
-        <Nav />
-        <button type="button" className="btn" onClick={this.getApi}>Score!</button>
-        <Dashboard 
-          gameTime={this.state.gameTime} 
-          awayTeam={this.state.awayTeam} 
-          homeTeam={this.state.homeTeam} 
-          awayScore={this.state.awayScore} 
-          homeScore={this.state.homeScore} 
->>>>>>> b4466aecbff7357d6f677dbc846ac15deb9aed49
         />
       </div>
     );
