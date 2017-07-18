989aaf0dcddb8e0e9d35e33235bb1d09bca43cb2
Chris Lee <pachopa0320@gmail.com>
2017-07-17 18:55:14 -0700

    create gameInfo file and take a game and showing on the sidebar

---
+++ src/GameInfo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class GameInfo extends React.Component {
+
+  render() {
+    return (
+     <div>
+       <a className="gameListColor"href="">{this.props.gameTime}  {this.props.gameTeam}</a>
+     </div>
+    );
+  }
+}
+
+
+export default GameInfo;
