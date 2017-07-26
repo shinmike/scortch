9f50b1a4e00efeaf5e9c7a75728a9349efc89781
shinmike <shinmike90@gmail.com>
2017-07-26 01:01:40 +0000

    login logout working

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -24,9 +24,7 @@ class Nav extends React.Component {
         password: this.state.password
       }
     }).then(function (response) {
-      console.log(response, "chris")
       const userInfo = JSON.parse(response.config.data);
-      //console.log(userInfo.email, response.config.data, "chris with michael")
       const user = {
         email: userInfo.email,
         password: userInfo.password
@@ -35,112 +33,130 @@ class Nav extends React.Component {
       self.props.loginModal();
     }).catch(function (error) {
       console.log(JSON.stringify(error))
-      console.log(error.response, "chris9090");
     });
   }
 
   email(event) {
-    //console.log(event.target.value, "chris email")
+    console.log("changing email email()");
     this.setState({ email: event.target.value });
-    //console.log(this.state.email, "chris-0-0");
   }
 
   password(event) {
-    console.log(event.target.value, "chris password")
     this.setState({ password: event.target.value });
   }
-  render() {
-    console.log("rendering <Nav>");
 
-    return (
-      <div>
-        <nav className="navbar navbar-light navbar-fixed-top navbarTop" id="my-navbar">
-          <div className="logo">Scortch 🔥</div>
+  handleLogout = (e) => {
+    e.preventDefault();
+    console.log("changing email logout()");
+    this.setState({user: {}});
+  }
 
-          <div className="container navBackground">
-            <div className="navbar-header">
-              <button type="button" className="navbar-toggle navSidebar" data-toggle="collapse" data-target="#navbarSidebar">
-                <span className="sr-only">Toggle navigation</span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-              </button>
-              <button type="button" className="navbar-toggle user-border" data-toggle="collapse" data-target="#navbar-collapse">
-                <span className="user-icon">
-                  <i className="fa fa-user" aria-hidden="true"></i>
-                </span>
-              </button>
 
-              <a href="" className="navbar-brand navTitle">Home</a>
+render() {
+  console.log("rendering <Nav>");
+
+  return (
+    <div>
+      <nav className="navbar navbar-light navbar-fixed-top navbarTop" id="my-navbar">
+        <div className="logo">Scortch 🔥</div>
+
+        <div className="container navBackground">
+
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle navSidebar" data-toggle="collapse" data-target="#navbarSidebar">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <button type="button" className="navbar-toggle user-border" data-toggle="collapse" data-target="#navbar-collapse">
+              <span className="user-icon">
+                <i className="fa fa-user" aria-hidden="true"></i>
+              </span>
+            </button>
+            <a href="" className="navbar-brand navTitle">Home</a>
+          </div>
+
+          {this.state.user.email ?
+
+            <div>
+              <h1>{this.state.user.email}</h1>
+              <button className="btn btn-primary navbar-right navRegister" onClick={this.handleLogout}>Logout</button>
             </div>
 
-            <div className="collapse navbar-collapse login-signup" id="navbar-collapse">
-              {/*sign in  */}
-              <button className="btn btn-primary navbar-right navRegister" onClick={this.props.loginModal}>Login</button>
-              <Modal isOpen={this.props.isActive} onRequestClose={this.loginModal}>
-                <form>
-                  <div className="col-lg-4 col-md-6 col-sm-8 outlineNone">
-                    <div className="row loginbox">
-                      <div className="col-lg-12 loginHeader">
-                        <ModalClose className="Modalclose" onClick={this.props.loginModal} />
-                        <span className="singtext" >Sign in </span>
+            :
+
+            <div>
+              <div className="collapse navbar-collapse login-signup" id="navbar-collapse">
+                {/*sign in  */}
+                <button className="btn btn-primary navbar-right navRegister" onClick={this.props.loginModal}>Login</button>
+                <Modal isOpen={this.props.isActive} onRequestClose={this.loginModal}>
+                  <form>
+                    <div className="col-lg-4 col-md-6 col-sm-8 outlineNone">
+                      <div className="row loginbox">
+                        <div className="col-lg-12 loginHeader">
+                          <ModalClose className="Modalclose" onClick={this.props.loginModal} />
+                          <span className="singtext" >Sign in </span>
+                        </div>
+                        <div className="spaceDiv"></div>
+                        <div className="col-lg-12 col-md-12 col-sm-12">
+                          <input onChange={this.email.bind(this)} type="submit" className="form-control" type="text" placeholder="E-mail" />
+                        </div>
+                        <div className="col-lg-12  col-md-12 col-sm-12">
+                          <span className="fa fa-envelope-o emailIcon" aria-hidden="true"></span>
+                          <input onChange={this.password.bind(this)} type="submit" className="form-control" type="password" placeholder="Password" />
+                        </div>
+                        <div className="col-lg-6  col-md-6 col-sm-6">
+                          <span className="fa fa-lock passwordIcon" aria-hidden="true"></span>
+                          <a href="#" className="btn loginPassword">Forgot your password? </a>
+                        </div>
+                        <div className="col-lg-6  col-md-6 col-sm-6 buttonTextAlign">
+                          <button onClick={this.handleLogin.bind(this)} type="submit" className="btn submitButton">Submit</button>
+                        </div>
                       </div>
-                      <div className="spaceDiv"></div>
+                    </div>
+                  </form>
+                </Modal>
+
+                {/* register  */}
+                <button className="btn btn-primary navbar-right navLogin" onClick={this.props.registerModal}>Register</button>
+                <Modal isOpen={this.props.isActive2} onRequestClose={this.registerModal}>
+                  <div className="col-lg-4 col-md-6 col-sm-8">
+                    <div className="row registerbox">
+                      <div className="col-lg-12">
+                        <ModalClose className="Modalclose" onClick={this.props.registerModal} />
+                        <span className="singtext" >Register</span>
+                      </div>
+                      <div className="regSpaceDiv"></div>
                       <div className="col-lg-12 col-md-12 col-sm-12">
-                        <input onChange={this.email.bind(this)} type="submit" className="form-control" type="text" placeholder="E-mail" />
+                        <span className="fa fa-users regiIcon" aria-hidden="true"></span>
+                        <input className="form-control" type="text" placeholder="Name" />
+                      </div>
+                      <div className="col-lg-12 col-md-12 col-sm-12">
+                        <span className="fa fa-envelope-o regiIcon" aria-hidden="true"></span>
+                        <input className="form-control" type="text" placeholder="E-mail" />
                       </div>
                       <div className="col-lg-12  col-md-12 col-sm-12">
-                        <span className="fa fa-envelope-o emailIcon" aria-hidden="true"></span>
-                        <input onChange={this.password.bind(this)} type="submit" className="form-control" type="password" placeholder="Password" />
+                        <span className="fa fa-lock regiIcon" aria-hidden="true"></span>
+                        <input className="form-control" type="password" placeholder="Password" />
                       </div>
-                      <div className="col-lg-6  col-md-6 col-sm-6">
-                        <span className="fa fa-lock passwordIcon" aria-hidden="true"></span>
+                      <div className="col-lg-12  col-md-12 col-sm-12">
                         <a href="#" className="btn loginPassword">Forgot your password? </a>
-                      </div>
-                      <div className="col-lg-6  col-md-6 col-sm-6 buttonTextAlign">
-                        <button onClick={this.handleLogin.bind(this)} type="submit" className="btn submitButton">Submit</button>
+                        <button href="#" className="btn  submitButton">Submit</button>
                       </div>
                     </div>
                   </div>
-                </form>
-              </Modal>
-
-              {/* register  */}
-              <button className="btn btn-primary navbar-right navLogin" onClick={this.props.registerModal}>Register</button>
-
-              <Modal isOpen={this.props.isActive2} onRequestClose={this.registerModal}>
-                <div className="col-lg-4 col-md-6 col-sm-8">
-                  <div className="row registerbox">
-                    <div className="col-lg-12">
-                      <ModalClose className="Modalclose" onClick={this.props.registerModal} />
-                      <span className="singtext" >Register</span>
-                    </div>
-                    <div className="regSpaceDiv"></div>
-                    <div className="col-lg-12 col-md-12 col-sm-12">
-                      <span className="fa fa-users regiIcon" aria-hidden="true"></span>
-                      <input className="form-control" type="text" placeholder="Name" />
-                    </div>
-                    <div className="col-lg-12 col-md-12 col-sm-12">
-                      <span className="fa fa-envelope-o regiIcon" aria-hidden="true"></span>
-                      <input className="form-control" type="text" placeholder="E-mail" />
-                    </div>
-                    <div className="col-lg-12  col-md-12 col-sm-12">
-                      <span className="fa fa-lock regiIcon" aria-hidden="true"></span>
-                      <input className="form-control" type="password" placeholder="Password" />
-                    </div>
-                    <div className="col-lg-12  col-md-12 col-sm-12">
-                      <a href="#" className="btn loginPassword">Forgot your password? </a>
-                      <button href="#" className="btn  submitButton">Submit</button>
-                    </div>
-                  </div>
-                </div>
-              </Modal>
+                </Modal>
+              </div>
             </div>
-          </div>
-        </nav>
-      </div>
-    );
-  }
+
+          }
+
+        </div>
+      </nav>
+    </div>
+  );
+}
 }
 
 export default Nav;
